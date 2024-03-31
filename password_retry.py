97df455b49d589086319f82883d03b56bf957fce
21godman <michaelkuo0617@gmail.com>
2024-03-31 14:59:57 +0800

    Teacher's optimization

--- password_retry.py
+++ password_retry.py
@@ -1,25 +1,25 @@
-times = 3 # 剩餘機會
-times = int(times)
-password = 'a123456'
-while True:
-	password = input('請輸入密碼: ')
-	if pwd == password:
-		print('登入成功')
-		break # 逃出迴圈
-	elif times != 0:
-		times = times - 1
-		print('密碼錯誤! 還有', times, '次機會')
-	else:
-		break
+# times = 3
+# times = int(times)
+# while True:
+# 	password = input('請輸入密碼: ')
+# 	if password == 'a123456':
+# 		print('登入成功')
+# 		break
+# 	elif times != 0:
+# 		times = times - 1
+# 		print('密碼錯誤! 還有', times, '次機會')
+# 	else:
+# 		break
 
-老師寫法
-while True:
-	password = input('請輸入密碼: ')
+# 老師寫法
+password = 'a123456'
+i = 3 # 剩餘機會
+
+while i > 0:
+	pwd = input('請輸入密碼: ')
 	if pwd == password:
 		print('登入成功')
 		break # 逃出迴圈
 	else:
-		times = times - 1
-		print('密碼錯誤! 還有', times, '次機會')
-		if times == 0:
-			break
+		i = i - 1
+		print('密碼錯誤! 還有', i, '次機會')
